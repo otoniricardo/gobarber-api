cac70416345fbe71d670082b41117cae3a5ddbfe
otoniricardo <otoniricardo@gmail.com>
2020-03-05 22:05:00 -0300

    Aula 16 - Middleware de autenticacao

--- src/app/controllers/User.js
+++ src/app/controllers/User.js
@@ -10,5 +10,9 @@ class UserController {
 
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
 }
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@ import { Router } from 'express';
 import UserController from './app/controllers/User';
 import SessionController from './app/controllers/Session';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
