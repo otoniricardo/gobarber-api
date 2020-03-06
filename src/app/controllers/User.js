1f11091ff82a3bc7ed44f813487ef2ef7ee10a80
otoniricardo <otoniricardo@gmail.com>
2020-03-05 22:15:52 -0300

    Aula 17 - Update do usuario

--- src/app/controllers/User.js
+++ src/app/controllers/User.js
@@ -12,7 +12,25 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    // return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+      if (userExists)
+        return res.status(400).json({ error: 'Email already in use' });
+    }
+
+    if (oldPassword && !(await user.checkpassowd(oldPassword)))
+      return res.status(401).json({ error: 'wrong passord' });
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, provider });
   }
 }
 export default new UserController();
